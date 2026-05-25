ffd3164bb453887d1883d429637412583b39d20a
itsketangupta <mrketanartist13@gmail.com>
2026-05-25 21:17:56 +0530

    nav box is going to complete not fully

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-const box = document.querySelector(".box");
+const video_box = document.querySelector(".video_box");
 
 window.addEventListener("scroll", () => {
 
     let scroll = window.scrollY;
-
-    // zoom
     let scale = 0.8 + (scroll * 0.0013);
-
     scale = Math.min(scale, 1.45);
-
-    // div keeps moving upward
     let moveY = scroll * 0.22;
-
-    box.style.transform =
-    `translate(-50%, calc(-50% - ${moveY}px)) scale(${scale})`;
+    video_box.style.transform = `translate(-50%, calc(-50% - ${moveY}px)) scale(${scale})`;
 
 });
